6610e14b298d9dd286d287f12c8d452ef72c1923
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-12-28 23:41:49 +0600

    created ui for login and sign up pages

---
+++ app/src/main/java/com/example/travelapp/view/SignUpFragment.kt
@@ -0,0 +1,20 @@
+package com.example.travelapp.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.travelapp.databinding.FragmentSignUpBinding
+
+class SignUpFragment : Fragment() {
+    private lateinit var binding: FragmentSignUpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =  FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
