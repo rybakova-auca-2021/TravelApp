26ade73d8e80340aded462959a117029fa540ef1
rybakova-auca-2021 <rybakova_k@auca.kg>
2023-12-31 13:56:54 +0600

    setup api for resetting password

--- app/src/main/java/com/example/travelapp/api/AuthInterface.kt
+++ app/src/main/java/com/example/travelapp/api/AuthInterface.kt
@@ -1,7 +1,10 @@
 package com.example.travelapp.api
 
+import com.example.travelapp.model.CodeVerificationModel
 import com.example.travelapp.model.LoginUser
 import com.example.travelapp.model.LoginUserResponse
+import com.example.travelapp.model.NewPasswordModel
+import com.example.travelapp.model.PasswordReset
 import com.example.travelapp.model.RegisterUser
 import com.example.travelapp.model.RegisterUserResponse
 import retrofit2.Call
@@ -18,4 +21,19 @@ interface AuthInterface {
     fun login(
         @Body request: LoginUser
     ) : Call<LoginUserResponse>
+
+    @POST("/api/password-reset/")
+    fun resetPassword(
+        @Body request: PasswordReset
+    ) : Call<Unit>
+
+    @POST("/api/verify-code/")
+    fun codeVerification(
+        @Body request: CodeVerificationModel
+    ) : Call<Unit>
+
+    @POST("/api/create-new-password/")
+    fun newPasswordCreate(
+        @Body request: NewPasswordModel
+    ) : Call<Unit>
 }
